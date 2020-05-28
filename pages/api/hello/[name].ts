2cc87755fb29c0c527bbb1845d15b27ae479fca1
ShigureTTE <g.vandenbiggelaar@gmail.com>
2020-05-28 14:27:05 +0200

    Update [name].ts

--- pages/api/hello/[name].ts
+++ pages/api/hello/[name].ts
@@ -2,5 +2,5 @@ import { NowRequest, NowResponse } from '@now/node'
 
 export default (request: NowRequest, response: NowResponse) => {
   const { name } = request.query
-  response.status(200).send(`Hello ${name}!`)
+  response.status(200).send(`Dag ${name}!`)
 }
